60a9e1260aad8204c35d303e7df3a50516d2f10d
Nathan <nathan.quan5@gmail.com>
2022-07-16 16:37:23 +1000

    fix

--- backend/src/api-server.py
+++ backend/src/api-server.py
@@ -350,7 +350,7 @@ def get_recipe():
 
 @api.route('/search/recipe/recipeid=<id>', methods=['GET'])
 @cross_origin()
-def get_recipe(id):
+def find_recipe(id):
     response = []
     cursor.execute("SELECT * FROM recipes where id = %s;", (id,))
     row = cursor.fetchone()
