ca9b607c7efbdd4eaad1fcd6e9095bc16f97cd1f
Edmond Lung <e.lung@student.unsw.edu.au>
2022-07-20 21:13:58 +1000

    loaded profile data onto frontend

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -7,16 +7,15 @@ import axios from 'axios';
 /* Profile Page */
 function Profile () {
 
+  const [userData, setuserData] = React.useState({});
+
   //Gets Auth Token
   const token = localStorage.getItem('token');
 
-
   const navigate = useNavigate();
-
   const changePassword = () => {
     navigate('/change-password');
   };
-
   const previous = () => {
 		navigate('/dashboard');
 	};
@@ -25,9 +24,15 @@ function Profile () {
     var headers = {
       "Authorization": `Bearer ${token}`
     }
-      console.log(headers);
     const response = await axios.get('http://localhost:5000/profile', {headers:headers});
-    console.log(response);
+    console.log(response.data.Email);
+      setuserData({
+        email: response.data.Email, 
+        username: response.data.Username, 
+        points: response.data.Points, 
+        bookmarks: response.data.Bookmarks
+      });
+      console.log(userData);
   }
 
   React.useEffect(() => {
@@ -41,6 +46,11 @@ function Profile () {
       <button onClick={previous}>Go Back</button>
         <div className="header">
           <h2>My Profile</h2>
+          <div className='userdata_field' >
+            <h4>Username: {userData.username}</h4>
+            <p>Email: {userData.email}</p>
+            <p>Points: {userData.points}</p>
+          </div>
         </div>
         <div className="info">
           <button onClick={() => changePassword()}>Change Password</button>
