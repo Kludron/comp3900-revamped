0020d20bac87fb4816597b1e6ec8459c55486394
Nathan <nathan.quan5@gmail.com>
2022-07-20 22:19:07 +1000

    working on implementation for our novel tasks

--- backend/src/api-server.py
+++ backend/src/api-server.py
@@ -453,5 +453,92 @@ def reviews(id):
 
     return jsonify(response)
 
+@api.route('/eaten/recipeid=<id>', methods=['POST'])
+@cross_origin()
+def eaten(id):
+    data = json.loads(request.get_data())
+    response = {}
+    
+    r_id = data["r_id"]
+    dateString = datetime.today().strftime('%d/%m/%Y')
+    u_id = null #to do: need jwt token stuff
+
+    #Note: need to add caloric values to ingredients
+    cursor.execute("INSERT INTO mealHistory(u_id, r_id, date) VALUES (%s, %s, %s);", (r_id, TO_DATE(dateString, 'DD/MM/YYYY')))
+
+    return (response, 200)
+
+@api.route('/intake_overview', methods=['GET'])
+@cross_origin()
+def IntakeOverview():
+    #Grain 
+    #Vegetables
+    #Fruit
+    #Dairy 
+    #Meat
+    #Fatty
+
+    data = json.loads(request.get_data())
+    response = {}
+    
+    r_id = data["r_id"]
+    u_id = null #to do: need jwt token stuff
+
+    #Combine with ingredients table. Limit to last 50 meals
+    cursor.execute("SELECT * from mealHistory(u_id, r_id, date) VALUES (%s, %s, %s);", (r_id, TO_DATE(dateString, 'DD/MM/YYYY')))
+
+    return (response, 200)
+
+@api.route('/recommend', methods=['GET'])
+@cross_origin()
+def recommend():
+    grainGoal =  33
+    vegetablesGoal = 16
+    fruitGoal = 16
+    dairyGoal = 15 
+    meatGoal = 12
+    fattyGoal = 7
+
+    response = []
+
+    #Combine with ingredients table. Limit to last 50 meals
+    cursor.execute("SELECT * from mealHistory(u_id, r_id, date) VALUES (%s, %s, %s);", (r_id, TO_DATE(dateString, 'DD/MM/YYYY')))
+    grain, vegetables, fruit, dairy, meat, fatty = getIntakeOverview()
+
+    goalDiff = {
+        "grain" : abs(grainGoal - grain),
+        "vegetables" : abs(vegetablesGoal - vegetables),
+        "fruit" : abs(fruitGoal - fruit),
+        "dairy" : abs(dairyGoal - dairy),
+        "meat" : abs(meatGoal - meat),
+        "fatty" : abs(fattyGoal - fatty)
+    }
+
+    #Imbalance if difference is > 15%
+    for key, value in goalDiff.items():
+        if value > 15:
+            response.append(key)
+    
+    #Returning a list of food categories that need improvement on
+    return response
+
+@api.route('/setGoal', methods=['POST'])
+@cross_origin()
+def setGoal():
+    data = json.loads(request.get_data())
+    response = {}
+    
+    try:
+        caloricGoal = data["goal"]
+    except KeyError():
+        return {"msg: wrong key", 401}
+
+    u_id = null #To do: jwt id stuff
+
+    #To do: need to add caloric values to ingredients
+    cursor.execute("UPDATE users SET goal = %s WHERE u_id = %s;", (goal, u_id))
+
+    return (response, 200)
+
 if __name__ == '__main__':
     api.run()
