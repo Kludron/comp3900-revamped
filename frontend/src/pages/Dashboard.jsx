b6cb2cafea0cd598149101f1390e4032a063ed30
bill0603 <52041537+bill0603@users.noreply.github.com>
2022-07-21 15:11:52 +1000

    fixed merge conflict

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -133,11 +133,7 @@ function Dashboard () {
         <MultipleSelect submit={(mealtypeName, cuisineName, ingredientsName, searchQuery) => {
           console.log('submitted');
           var body = {
-<<<<<<< HEAD
             "search": searchQuery,
-=======
-            "search": "sal",
->>>>>>> 90e2b0a16e527be44f15b28fff6263a6b7317df2
             "mealTypes": mealtypeName,
             "cuisines": cuisineName,
             "ingredients": ingredientsName
