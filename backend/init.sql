3ddb9743a9d7adb5d9734f2855cedb751ae79c1b
5256175 <z5256175@ad.unsw.edu.au>
2022-07-23 16:01:22 +1000

    updated ingredients

--- backend/init.sql
+++ backend/init.sql
@@ -1,82 +1,86 @@
 create table Users (
-    id            int unique GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
-    username      varchar(200) unique not null,
-    pass_hash     text not null,
-    email         text unique not null,
-    points        int default 0,
-    primary key   (id)
+    id              int unique GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
+    username        varchar(200) unique not null,
+    pass_hash       text not null,
+    email           text unique not null,
+    points          int default 0,
+    primary key     (id)
 );
 
 create table Cuisines (
-    id            int unique not null,
-    name          text unique not null,
-    primary key   (id)
+    id              int unique not null,
+    name            text unique not null,
+    primary key     (id)
 );
 
 create table MealTypes (
-    id            int unique not null,
-    name          text unique not null,
-    primary key   (id)
+    id              int unique not null,
+    name            text unique not null,
+    primary key     (id)
 );
 
 create table Ingredients (
-    id            int unique GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
-    name          text unique not null,
-    calories      float,  -- Per 100g
-    fat           float,  -- Per 100g
-    sodium        float,  -- Per 100g
-    carbohydrates float,  -- Per 100g
-    fiber         float,  -- Per 100g
-    sugars        float,  -- Per 100g
-    protein       float,  -- Per 100g
-    primary key   (id)
+    id              int unique GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
+    name            text unique not null,
+    energy          float default 0.0, -- (kJ)
+    protein         float default 0.0, -- (g)
+    fat             float default 0.0, -- (g)
+    fibre           float default 0.0, -- (g)
+    sugars          float default 0.0, -- (g)
+    carbohydrates   float default 0.0, -- (g)
+    calcium         float default 0.0, -- (mg)
+    iron            float default 0.0, -- (mg)
+    magnesium       float default 0.0, -- (mg)
+    manganese       float default 0.0, -- (mg)
+    phosphorus      float default 0.0, -- (mg)
+    primary key     (id)
 );
 
 create table Recipes (
-    id            int unique GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
-    name          varchar(200) not null,
-    description   text not null,
-    cuisine       int references Cuisines(id),
-    mealType      int references MealTypes(id),
-    servingSize   int not null,
-    uploader      int references Users(id) default 0,
-    primary key   (id)
+    id              int unique GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
+    name            varchar(200) not null,
+    description     text not null,
+    cuisine         int references Cuisines(id),
+    mealType        int references MealTypes(id),
+    servingSize     int not null,
+    uploader        int references Users(id) default 0,
+    primary key     (id)
 );
 
 create table recipe_ingredients (
-    r_id          int references Recipes(id) not null,
-    ingredient    int references Ingredients(id) not null,
-    quantity      float,  -- E.g. 2 Lemons
-    grams         float,
-    millilitres   float,
-    primary key   (r_id, ingredient)
+    r_id            int references Recipes(id) not null,
+    ingredient      int references Ingredients(id) not null,
+    quantity        float,  -- E.g. 2 Lemons
+    grams           float,
+    millilitres     float,
+    primary key     (r_id, ingredient)
 );
 
 create table user_upvotes (
-    u_id          int references Users(id) not null,
-    r_id          int references Recipes(id) not null,
-    primary key   (r_id, u_id)
+    u_id            int references Users(id) not null,
+    r_id            int references Recipes(id) not null,
+    primary key     (r_id, u_id)
 );
 
 create table user_bookmarks (
-    u_id          int references Users(id) not null,
-    r_id          int references Recipes(id) not null,
-    primary key   (u_id, r_id)
+    u_id            int references Users(id) not null,
+    r_id            int references Recipes(id) not null,
+    primary key     (u_id, r_id)
 );
 
 create table user_recentlyViewed (
-    u_id          int references Users(id) not null,
-    r_id          int references Recipes(id) not null,
-    primary key   (u_id, r_id)
+    u_id            int references Users(id) not null,
+    r_id            int references Recipes(id) not null,
+    primary key     (u_id, r_id)
 );
 
 create table Comments (
-    c_id          int GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
-    r_id          int references Recipes(id) not null,
-    u_id          int references Users(id) not null,
-    description   text not null,
-    parent        int references Comments(c_id),
-    primary key   (c_id)
+    c_id            int GENERATED ALWAYS AS IDENTITY (START WITH 1) not null,
+    r_id            int references Recipes(id) not null,
+    u_id            int references Users(id) not null,
+    description     text not null,
+    parent          int references Comments(c_id),
+    primary key     (c_id)
 );
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO comp3900_user;
