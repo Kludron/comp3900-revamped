f10ab66e5d90a3d40239cea4280c7a26f7eb83f6
z5256175 <z5256175@ad.unsw.edu.au>
2022-08-01 23:21:16 +1000

    myrecipes frontend fix?

--- frontend/src/pages/MyRecipes.jsx
+++ frontend/src/pages/MyRecipes.jsx
@@ -5,7 +5,7 @@ import './MyRecipes.css';
 import Button from '@mui/material/Button';
 import axios from "axios";
 
-// axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
 /* MyRecipes Page */
 function MyRecipes() {
@@ -27,7 +27,7 @@ function MyRecipes() {
 		let headers = {
 			"Authorization": `Bearer ${token}`,
 		}
-		axios.get('http://localhost:5000/my-recipes', { headers: headers })
+		axios.get('http://localhost:5000/my-recipes')
 		.then((response) => {
 			setRecentViewed([]);
 			console.log(response);
@@ -52,7 +52,7 @@ function MyRecipes() {
 			"Authorization": `Bearer ${token}`
 		}
 		// axios.get('http://localhost:5000/recentlyviewed', body, { headers: headers })
-		axios.get('http://localhost:5000/recentlyviewed', body, { headers: headers })
+		axios.get('http://localhost:5000/recentlyviewed', body)
 		.then((response) => {
 			console.log(response);
 		}).catch(err => {
