1d01bb4a9fa85cc1849ed46b784d763c3fae05b1
Nathan <nathan.quan5@gmail.com>
2022-08-04 14:27:52 +1000

    client_secret fixed

--- backend/src/utils/gmail/gmail_auth.py
+++ backend/src/utils/gmail/gmail_auth.py
@@ -15,7 +15,7 @@ import google.auth
 from googleapiclient.errors import HttpError
 # This variable specifies the name of a file that contains the OAuth 2.0
 # information for this application, including its client_id and client_secret.
-CLIENT_SECRETS_FILE = "backend/src/utils/gmail/client_secret.json"
+CLIENT_SECRETS_FILE = "client_secret.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
