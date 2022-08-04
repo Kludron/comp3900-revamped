951169e97756a98a581d12f1971cb5341fe9945d
Edmond Lung <e.lung@student.unsw.edu.au>
2022-08-04 14:25:54 +1000

    favourites fixed

--- frontend/src/pages/Favourite.jsx
+++ frontend/src/pages/Favourite.jsx
@@ -4,47 +4,36 @@ import './Favourite.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Button } from "@mui/material";
 import profile_background from '../img/profile-background.jpeg';
+import Button from '@mui/material/Button';
 
-const token = localStorage.getItem('token');
 /* Favourite Recipes Page */
 function Favourite() {
+  const token = localStorage.getItem('token');
+  const [bookmarks, setBookmarks] = useState([]);
 
-  const navigate = useNavigate();
-  const previous = () => {
-    navigate('/dashboard');
-  };
-
-  const [recipes, setRecipes] = useState([]);
-  const [bookmarkIds, setBookmarkIds] = useState([]);
-
-  const loadFavourite = async () => {
+  const loadBookmarks = async () => {
     var headers = {
       "Authorization": `Bearer ${token}`
     }
     const response = await axios.get('http://localhost:5000/favourite', {headers:headers});
-    setRecipes(response.data.Bookmarks);
-    setBookmarkIds(response.data.b_id)
-  };
-  
-  React.useEffect(() => {
-    loadFavourite();
-  }, [])
-  console.log(recipes)
-  console.log(bookmarkIds)
-  
+    console.log(response.data.Bookmarks);
+      setBookmarks(response.data.Bookmarks);
+  }
+
+  useEffect(() => {
+    loadBookmarks();
+  }, []);
+
   return <div>
     <img className='profile-background' src={profile_background} alt='profile background'></img>
     <NavBar />
     <div className="main-content">
-      <Button className="go-back" variant='contained' onClick={previous}>← Go Back</Button>
-      <h1>Favourite Recipe</h1>
-      <hr className="favourite-hr"/>
-      <div className="list_bookmarked_recipes">
-        {recipes.map((recipe, key) => {
+      <h2>Favourite Recipe</h2>
+      <div>
+        {bookmarks.map((recipe, key) => {
           return (
-            <BookmarkedRecipe key={key} recipe={recipe} bookmarkIds={bookmarkIds} setBookmarkIds={setBookmarkIds}/>
+            <BookmarkedRecipe key={key} recipe={recipe} />
           )
         })}
       </div>
@@ -54,73 +43,39 @@ function Favourite() {
 
 export default Favourite;
 
-function BookmarkedRecipe({ key, recipe, bookmarkIds, setBookmarkIds }) {
-  console.log(recipe)
+function BookmarkedRecipe(r) {
+  console.log(r.recipe)
+  const recipe = r.recipe
+  const [favourite, setfavourite] = useState(false);
+  const [bookmarkStar, setbookmarkStar] = useState('★');
   const navigate = useNavigate();
 
-  const viewRecipe = (recipeid) => {
+  const viewRecipe = ({ recipeid }) => {
+    console.log(recipeid);
     navigate(`/view/recipe/${recipeid}`);
   }
 
-  const handleBookmark = async (id) => {
-    var headers = {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
-    }
-    var body = {
-      id, 
-      bookmarkIds
-    };
-    axios.put('http://localhost:5000/favourite', body, { headers: headers })
-    .then((response) => {
-      console.log(response)
-      setBookmarkIds(response.data.Bookmarks)
-    }).catch((error) => {
-      alert(error)
-    });
-  };
-
-  const showBookmark = (id) => {
-    console.log(bookmarkIds)
-    if (bookmarkIds.includes(id)) {
-      return '★'
-    } else {
-      return '☆'
-    }
-  };
-
-  const eatenRecipe = async (recipeid) => {
-    if(localStorage.getItem('token') != null){
-      var headers = {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      }
-      let body = {
-        r_id: recipeid,
-      }
-      console.log(body);
-      const response = await axios.put(`http://localhost:5000/eaten/recipeid=${recipeid}`, body, {headers:headers})
-      console.log(response.status);
-      if(response.status === '200'){
-        alert('You have marked the recipe as eaten.');
-      } else {
-        alert('An issue has occurred marking the recipe as eaten. Please try again.')
-      }
-    } else {
-      alert('Please create an account to access the Eaten functionality and being tracking your dietary intake.')
+  const handleBookmark = () => {
+    if (bookmarkStar === '☆' && favourite === false) { //Bookmarked
+      setfavourite(true);
+      setbookmarkStar('★');
+      console.log('bookmarked'); //Still need to work out how to store this state and send state to backend
+    } else { //Un-bookmarked
+      setfavourite(false);
+      setbookmarkStar('☆');
+      console.log('unbookmarked'); //As above
     }
   }
 
-  return <div className='recipe_box' key={key}>
-    <Button className="btn" variant="outlined" onClick={() => handleBookmark(recipe.id)}>{showBookmark(recipe.id)}</Button>
-    <Button className='btn' variant="outlined" onClick={() => eatenRecipe(recipe.id)}>Eaten</Button>
-    <div className="details">
-      <h3 className="rec_name">{recipe.name}</h3>
+  return <div className='recipe_box' key={recipe.id}>
+    <Button class='bookmark' variant='outlined' onClick={() => handleBookmark()}>{bookmarkStar}</Button>
+      <div className='details'>
+      <h3>Name: {recipe.name}</h3>
       <p>Cuisine: {recipe.cuisine}</p>
       <p>Description: {recipe.description}</p>
       <p>Mealtype: {recipe.mealType}</p>
-      <p>Serves: {recipe.servingSize}</p>
-      <Button variant="contained" className='see_recipe_button' onClick={() => viewRecipe(recipe.id, key)}>View Recipe→</Button>
+      <p>Serving Size: {recipe.servingSize}</p>
     </div>
+    <Button variant='contained' className='see_recipe_button' onClick={() => viewRecipe(recipe.id)}>See Recipe→</Button>
   </div>
 }
